83954d5d8da76fb40e253d3e694b7ba49a5f9cb2
Kevin Huddleston <huddlke@pascal.cs.longwood.edu>
2019-11-18 14:41:14 -0500

    small change to main

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ txt = open('can_votes.txt', 'w')
 with open('2019electionsum.csv', "r") as fitem:
     filereader = csv.reader(fitem)
     for line in filereader:
-        print(line[9],line[10],line[20], file = txt)
+        print(line[9],line[10],line[17],line[20], file = txt)
     #print(result[9],result result[total_votes])
         #print(line[9],line[10],line[17],line[20])
         
