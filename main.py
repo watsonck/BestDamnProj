faa462973370be71de26feda472390cb019489b0
Kevin Huddleston <huddlke@pascal.cs.longwood.edu>
2019-11-18 12:39:19 -0500

    got names and votes for each thick boi

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import csv
 import sys
 
 result = []
-with open(file name, "r") as fitem:
+txt = open('can_votes.txt', 'w')
+with open('2019electionsum.csv', "r") as fitem:
     filereader = csv.reader(fitem)
     for line in filereader:
-        result.append(line)
-
-    print(result)
+        print(line[9],line[10],line[20], file = txt)
+    #print(result[9],result result[total_votes])
+        #print(line[9],line[10],line[17],line[20])
+        
