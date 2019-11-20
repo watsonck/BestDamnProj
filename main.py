60e19768ebe15d806dbbce9a4fdf58ecde755490
Kevin Huddleston <huddlke@pascal.cs.longwood.edu>
2019-11-20 12:46:17 -0500

    Changed main to create a file for 2019NovGen.csv

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import csv
 import sys
 
-result = []
-txt = open('can_votes.txt', 'w')
-with open('2019electionsum.csv', "r") as fitem:
-    filereader = csv.reader(fitem)
-    for line in filereader:
-        print(line[9],line[10],line[17],line[20], file = txt)
-    #print(result[9],result result[total_votes])
-        #print(line[9],line[10],line[17],line[20])
+def elecinfo():
+    txt = open('can_votes.txt', 'w')
+    with open('2019NovGen.csv', "r") as fitem:
+        filereader = csv.reader(fitem)
+        for line in filereader:
+            print(line[5],line[6],line[9], file = txt)
         
+
+
+#TODO create decorator add all democrat, republican and independent votes per locality
