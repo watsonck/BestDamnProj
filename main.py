2da9e790ee356ec0b6f29000512f8e69a08a3f36
Kevin Huddleston <huddlke@pascal.cs.longwood.edu>
2019-12-02 11:33:51 -0500

    Made dictionaries that added all the votes of the same party and locality together for each gen elec

--- main.py
+++ main.py
@@ -13,39 +13,20 @@ def elecinfo(fname):
                 line[5] = int(line[5])
                 info.append([line[5],line[6],line[9]])
     return info
-#added line 20 to the first for loop
-'''
-def rembad(oldlst):
-    newlst = []
-    for line in oldlst:
-        if line[1] != '':
-            newlst.append(line)
-            int(newlst[[0]])
-    return newlst
-'''
-
-def memoize(f):
-    memo = {}
-    def helper(x):
-        if x not in memo:
-            memo[x] = f(x)
-        return memo[x]
-    return helper
-
 
 def dictMaker(oldlst):
     newdict = {}
     for el in oldlst:
         newdict.update({(el[1],el[2]) : el[0]})
-         
                 
     return newdict
         
+def fileMaker(lst, fil):
+    ofil = open(fil,'w')
+    print(lst,file=ofil)
+    ofil.close()
 
 
-
-
-#TODO create decorator add all democrat, republican and independent votes per locality
 
 info19 = elecinfo("2019NovGen.csv")
 info18 = elecinfo("2018NovGen.csv")
@@ -53,13 +34,21 @@ info17 = elecinfo("2017NovGen.csv")
 info16 = elecinfo("2016NovGen.csv")
 
 format16 = dictMaker(info16)
-fil = open("regfile16.txt", "w")
-print(format16, file=fil)
-fil.close()
+format17 = dictMaker(info17)
+format18 = dictMaker(info18)
+format19 = dictMaker(info19)
+
+toGraph16 = fileMaker(format16, 'regfile16.txt')
+toGraph17 = fileMaker(format17, 'regfile17.txt')
+toGraph18 = fileMaker(format18, 'regfile18.txt')
+toGraph19 = fileMaker(format19, 'regfile19.txt')
+
+
+
         
-infofil = open('can_votes.txt','w')
+#infofil = open('can_votes.txt','w')
 
 
-for i in range(0,len(info16)-1):
-    print(info16[i], file = infofil)
-infofil.close()
+#for i in range(0,len(info16)-1):
+#    print(info16[i], file = infofil)
+#infofil.close()
