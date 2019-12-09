91371791fdf5dd0347125f25de737bf277ae4b09
Colin Watson <watsonck@stroustrup.cs.longwood.edu>
2019-12-08 20:23:03 -0500

    more stuff, WORKING DEMO

--- main.py
+++ main.py
@@ -36,23 +36,10 @@ def DictMaker(oldlst):
             newdict.update({(el[1],el[2]) : el[0]})
     return newdict
         
-#Makes file for the dictionary
-def fileMaker(lst, fil):
-    ofil = open(fil,'w')
-    print(lst,file=ofil)
-    ofil.close()
-
-
 info19 = elecinfo("2019NovGen.csv")
 info18 = elecinfo("2018NovGen.csv")
 info17 = elecinfo("2017NovGen.csv")
 info16 = elecinfo("2016NovGen.csv")
-'''
-toGraph16 = fileMaker(format16, 'regfile16.txt')
-toGraph17 = fileMaker(format17, 'regfile17.txt')
-toGraph18 = fileMaker(format18, 'regfile18.txt')
-toGraph19 = fileMaker(format19, 'regfile19.txt')
-'''
 
 #Gather user input
 print("what year would you like? [all][2019][2018][2017][2016]")
@@ -60,11 +47,12 @@ year = input()
 print("State or locatality? [state][###]")
 area = input()
 
-print(year)
-print(area)    
 infofil = open('can_votes.txt','w')
 
+
+
 #if they want all the years of the state
+#Stacked bar chart
 if year == "all" and area == "state":
     format19 = stateDictMaker(info19)
     format18 = stateDictMaker(info18)
@@ -114,7 +102,7 @@ if year == "all" and area == "state":
 
     ax.set_ylabel('Votes')
     
-    ax.set_title('Virginia Party voytes through the Year')
+    ax.set_title('Virginia Party votes through the Year')
     ax.set_xticks(x)
     ax.set_xticklabels(labels, fontsize = 6)
     ax.legend()
@@ -125,7 +113,10 @@ if year == "all" and area == "state":
 
 #-----------------------------------------------------------------------------------
 #Specific year in the state
+#some chart
 if year != "all" and area == "state":
+    
+    #2019---------------------------------------------------------------------------
     if year == "2019":
         format19 = stateDictMaker(info19)
         
@@ -146,10 +137,73 @@ if year != "all" and area == "state":
         ax.axis('equal')
         plt.show()
 
+    #2018---------------------------------------------------------------------------
+    if year == "2018":
+        format18 = stateDictMaker(info18)
+        
+        labels = format18.keys()
+        val = list(format18.values())
+        total = 0
+        for item in val:
+            total = item + total
+
+        value = []
+        for item in val:
+            newVal = item/total
+            value.append(newVal)
+        #fig1, ax = plt.subplots()
+        patches, texts = plt.pie(value, shadow = False, startangle = 0)
+        #patches, texts =ax.pie(val, labels = labels, autopct = '%1.1f%%',shadow = False, startangle= 0)
+        plt.legend(patches, labels, loc = 'best')
+        ax.axis('equal')
+        plt.show()
+
+    #2017---------------------------------------------------------------------------
+    if year == "2017":
+        format17 = stateDictMaker(info17)
+        
+        labels = format17.keys()
+        val = list(format17.values())
+        total = 0
+        for item in val:
+            total = item + total
+
+        value = []
+        for item in val:
+            newVal = item/total
+            value.append(newVal)
+        #fig1, ax = plt.subplots()
+        patches, texts = plt.pie(value, shadow = False, startangle = 0)
+        #patches, texts =ax.pie(val, labels = labels, autopct = '%1.1f%%',shadow = False, startangle= 0)
+        plt.legend(patches, labels, loc = 'best')
+        ax.axis('equal')
+        plt.show()
+
+    #2016---------------------------------------------------------------------------
+    if year == "2016":
+        format16 = stateDictMaker(info16)
+        
+        labels = format16.keys()
+        val = list(format16.values())
+        total = 0
+        for item in val:
+            total = item + total
+
+        value = []
+        for item in val:
+            newVal = item/total
+            value.append(newVal)
+        #fig1, ax = plt.subplots()
+        patches, texts = plt.pie(value, shadow = False, startangle = 0)
+        #patches, texts =ax.pie(val, labels = labels, autopct = '%1.1f%%',shadow = False, startangle= 0)
+        plt.legend(patches, labels, loc = 'best')
+        ax.axis('equal')
+        plt.show()
 
 
 #-------------------------------------------------------------------
 # All years in a Locality
+#grouped bar chart
 if year == "all" and area != "state":
     format19 = DictMaker(info19)
     format18 = DictMaker(info18)
@@ -171,8 +225,6 @@ if year == "all" and area != "state":
         if item[1] == area:
             overDict.update({('2016',item[0],item[1]):format16[item]})
 
-    #rint(overDict)
-
     labels = ['Republican','Democratic','Independent', 'Green', 'Libertarian','Write-In']
     value19 = [0,0,0,0,0,0,0]
     value18 = [0,0,0,0,0,0,0]
@@ -190,7 +242,6 @@ if year == "all" and area != "state":
             value16[labels.index(item[1])]= overDict[item]
 
  
-    #pull up bar graph for locality
     value19 = np.array(value19)
     value18 = np.array(value18)
     value17 = np.array(value17)
@@ -220,7 +271,10 @@ if year == "all" and area != "state":
 
 #-------------------------------------------------------------------
 #Specific year in Locality
+#pie Chart
 if year != "all" and area != "state":
+
+    #2019----------------------------------------------------------
     if year == "2019":
         format19 = DictMaker(info19)
 
@@ -237,6 +291,8 @@ if year != "all" and area != "state":
         #plt.legend(patches, labels, loc = 'best')
         ax.axis('equal')
         plt.show()
+
+    #2018 ----------------------------------------------------------
     if year == "2018":
         format18 = DictMaker(info18)
 
@@ -253,7 +309,9 @@ if year != "all" and area != "state":
         #plt.legend(patches, labels, loc = 'best')
         ax.axis('equal')
         plt.show()
-     
+    
+
+    #2017 ----------------------------------------------------------
     if year == "2017":
         format17 = DictMaker(info17)
 
@@ -271,6 +329,8 @@ if year != "all" and area != "state":
         ax.axis('equal')
         plt.show()
     
+    
+    #2016 ----------------------------------------------------------
     if year == "2016":
         format16 = DictMaker(info16)
 
@@ -292,32 +352,4 @@ if year != "all" and area != "state":
 for i in range(0,len(info16)-1):
     print(info16[i], file = infofil)
 infofil.close()
-'''
-print(format19)
-print(format18)
-print(format17)
-print(format16)
-'''
-#making graphs
-'''
-labels19 = format19.keys()
-values19 = list(format19.values())
-index19 = np.arange(len(labels19))
-plt.bar(index19, values19)
-plt.xlabel("political party")
-plt.ylabel("# of votes")
-plt.xticks(index19, labels19)
-plt.title("2019 party comparison")
-plt.show()
 
-labels18 = ['Independent','Republican','Democratic','Libertarian','Write-In']
-values18 = [format18['Independent'],format18['Republican'],format18['Democratic'],format18['Libertarian'],format18['Write-In']]
-
-index18 = np.arange(len(labels18))
-plt.bar(index18, values18)
-plt.xlabel("political party")
-plt.ylabel("# of votes")
-plt.xticks(index18, labels18)
-plt.title("2018 party comparison")
-plt.show()
-'''
